fa051155088e1feddda57ef9afa822eeef46b3e1
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-02-24 13:25:09 +0530

    deployment config

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://todolist.up.railway.app/api',
 };
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-// const connectionUrl = "mongodb+srv://admin:fnurq8EzMN8JH7sV@cluster0.tpeau.mongodb.net/budget-manager?retryWrites=true&w=majority";
-const connectionUrl = "mongodb://127.0.0.1:27017/todo"; // local connection url
+const connectionUrl = "mongodb+srv://admin:kAH410WsHVgqDdwE@cluster0.i0z13er.mongodb.net/?retryWrites=true&w=majority";
+// const connectionUrl = "mongodb://127.0.0.1:27017/todo"; // local connection url
 
 mongoose.set("strictQuery", false);
 mongoose.connect(
