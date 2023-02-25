29a67e4d8bc2a5b20127c7f42179eae4d8ba1f6a
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-02-25 21:10:08 +0530

    fix

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -6,6 +6,7 @@ const taskController = require("../controllers/taskController");
 router.post("/getBoard", taskController.getBoard);
 router.post("/updateBoard", taskController.updateBoard);
 router.post("/newTask", taskController.newTask);
+router.post("/newSubtask", taskController.newSubTask);
 router.post("/isCompleteSubtask", taskController.isCompleteSubtask);
 
 module.exports = router;
